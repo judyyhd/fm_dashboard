c1c1ecfa8d01d13d4933fae94e1cff71b6f48397
Judy Yang <109549280+judyyhd@users.noreply.github.com>
2025-07-07 10:11:47 +0800

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,7 @@
 import type { AppProps } from 'next/app'
-import { DashboardProvider } from '../contexts/DashboardContext'
+// import { DashboardProvider } from '../contexts/DashboardContext'
 import '../index.css' // If you have global styles
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <DashboardProvider>
-      <Component {...pageProps} />
-    </DashboardProvider>
-  )
+  return <Component {...pageProps} />
 }
