5a1e6a1ce30c9e950dd8f71ea24952f94e5d4159
Judy Yang <109549280+judyyhd@users.noreply.github.com>
2025-07-08 10:35:25 +0800

    Update dashboard.tsx

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Zap, Shield, Wrench, Boxes } from 'lucide-react';
 
-const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#8884d8', '#82ca9d', '#ffc658', '#ff7300'];
 
 const Dashboard = () => {
   const [dashboardData, setDashboardData] = useState(null);
